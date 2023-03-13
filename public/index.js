bced8e140ba37eb073a978ecad466fae46a2367f
Muhammad Huzaifa <hajihuzaifa9612@gmail.com>
2023-03-13 19:37:04 +0500

    copy issue resolved

--- public/index.js
+++ public/index.js
@@ -2,8 +2,15 @@ console.log('running');
 
 const addEmail = ()=>{
     try {
-        navigator.clipboard.writeText('28008@students.riphah.edu.pk');
-        alert('Email copied to clipboard');
+        if(!navigator.clipboard){
+            alert('clipboard unabled');
+            document.getElementById('clipboardUnabled').style.display = 'block';
+            
+        }
+        else{
+            navigator.clipboard.writeText('28008@students.riphah.edu.pk');
+            alert('Email copied to clipboard');
+        }
     } catch (error) {
         
     }
@@ -11,8 +18,15 @@ const addEmail = ()=>{
 
 const addWhatsApp = ()=>{
     try {
-        navigator.clipboard.writeText('03110644893');
-        alert('Whatsapp copied to clipboard');
+        if(!navigator.clipboard){
+            document.getElementById('clipboardUnabled').style.display = 'block';
+            alert('clipboard unable');
+        }
+        else{
+            navigator.clipboard.writeText('03110644893');
+            alert('Whatsapp copied to clipboard');
+
+        }
         
     } catch (error) {
         console.log(error)
